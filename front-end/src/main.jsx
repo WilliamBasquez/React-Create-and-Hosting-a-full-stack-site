a022babc1b64931bf347b0c6d45a7ac2cfc070e7
William Basquez <william.basquez@visualinterest.com>
2025-11-24 07:30:52 -0700

    Configured Firebase Auth.

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import firebaseConfig from "./firebaseConfig.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
