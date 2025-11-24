62d2f6b9883131a799df9d0d4d1499ea5deaf62c
William Basquez <william.basquez@visualinterest.com>
2025-11-24 07:25:43 -0700

    Created custom hook to check for authentication.

---
+++ front-end/src/useUser.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const useUser = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(getAuth(), (currentUser) => {
+            setUser(currentUser);
+            setIsLoading(false);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    return {isLoading, user};
+}
+
+export default useUser;
